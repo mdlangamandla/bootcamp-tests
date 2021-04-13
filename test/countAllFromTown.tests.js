8ce8fc66404c9ec5f164a84dca635cf69ccd81f8
mdlangamandla <mdlangamandla048@gmail.com>
2021-04-13 16:17:16 +0200

    update

--- test/countAllFromTown.tests.js
+++ test/countAllFromTown.tests.js
@@ -2,8 +2,10 @@ describe('The countAllFromTown function', function(){
     it('should return 3, the total of the registration numbers from Stellies.', function(){    
         assert.deepEqual(countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'), 3)
     });
-
-   
+    it('should return 0 if registration number does not start with CL' , function(){
+        var fromStellies = countAllFromTown('CJ 456,CY 567, CJ 123 ','CL');
+        assert.deepEqual(fromStellies, 0)
+    });   
 })
 
 
--- test/findItemsOver.tests.js
+++ test/findItemsOver.tests.js
@@ -16,17 +16,16 @@ describe('The findItemsOver function', function() {
     
     
     ];
-    
+
+    var threshold = 20;
     it('should return items over the threshold', function() {
-        var threshold = 20;
-        assert.deepEqual([{name : 'oranges', qty : 37},
+         assert.deepEqual([{name : 'oranges', qty : 37},
         {name : 'mangos', qty : 27}], findItemsOver(itemList, threshold));
     })
 
 
-     
+    var threshold = 20;     
     it('should return an empty array if there are no items on the threshold', function() {
-        var threshold = 20;
         assert.deepEqual([], findItemsOver(itemList2, threshold));
     })
 });
