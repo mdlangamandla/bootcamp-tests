9a63df8709779ee51af2588d5eaa0ab228ea5e8a
mdlangamandla <mdlangamandla048@gmail.com>
2021-04-14 09:51:12 +0200

    update

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -1,10 +1,10 @@
 describe('The yearsAgo function', function(){
-    it('should return 21 which is the diffYears between the current year and 2000.', function(){
+    it('should return 21 which is the diff in Years between the current year and the year 2000.', function(){
         assert.equal(21, yearsAgo(2000));
     })
 
   
-    it('should return 0 diff if we are in current year.', function(){
+    it('should return 0 which is the diff in years if we are in current year.', function(){
         assert.equal(0, yearsAgo(2021));
     })
 })
